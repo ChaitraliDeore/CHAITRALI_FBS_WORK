bcdda6ea6cf01ad048aa1cbcbba3b67f98c504ea
Chaitrali Deore <deorechaitrali26@gmail.com>
2025-09-09 12:27:08 +0530

    Add files via upload

---
+++ C-Programming/Assignment 5/4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int space=i;space<5;space++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+}
---
+++ C-Programming/Assignment 5/8.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int space=i;space<5;space++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=(2*i-1);j++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+     for(int i=5;i>=1;i--)
+    {
+        for(int space=i;space<5;space++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=(2*i-
+            1);j++)
+        {
+
+            printf("*");
+        }
+        printf("\n");
+    }
+}
